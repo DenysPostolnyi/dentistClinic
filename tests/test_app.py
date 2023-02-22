dfc63b952d1db0cd2597114392f1667b1493b26c
DenysPostolnyi <postlden44@gmail.com>
2023-02-22 19:40:43 +0200

    created test for doctor service

---
+++ tests/test_app.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.models.models import db, Doctor, Specialty
+from src.service import doctor_service
+from src import create_app
+
+
+class TestDB(unittest.TestCase):
+    def test_get_doctor(self):
+        with create_app().app_context():
+            self.assertIsNotNone(doctor_service.get_one_by_id(2))
