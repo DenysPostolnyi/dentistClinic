a34d9ceb4619e79a323b8113b14708cb16cd0356
DenysPostolnyi <postlden44@gmail.com>
2023-02-22 12:20:37 +0200

    data base has been created

--- src/setup.py
+++ src/setup.py
@@ -3,20 +3,16 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-
 setup(
     name='dentistClinic',
     version='1.0',
     description='Application for dentist clinic',
-    packages=[
-        'service',
-        'rest',
-        'models',
-        'migrations'
-    ],
+    # packages=find_packages(where='models', exclude='tests'),
+    packages=['models', 'service', 'migrations'],
     author='Denys Postolnyi',
     author_email='poostlden44@gmail.com',
     include_package_data=True,
-    zip_safe=False,
+    # zip_safe=False,
     install_requires=requirements
 )
+
