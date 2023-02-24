465053de2d497e70b3fd4afa3224aae0d5e601e9
DenysPostolnyi <postlden44@gmail.com>
2023-02-24 07:32:46 +0200

    travic DB

--- src/config.py
+++ src/config.py
@@ -13,6 +13,6 @@ class DBSettings:
     """
     DATABASE = os.getenv('MYSQL_DATABASE')
     USERNAME = os.getenv('MYSQL_USERNAME')
-    PASSWORD = os.getenv('MYSQL_PASSWORD')
+    PASSWORD = os.getenv('MYSQL_PASSWORD') if os.getenv('MYSQL_PASSWORD') else ""
     PORT = os.getenv('MYSQL_PORT')
     SQLALCHEMY_DATABASE_URL = "mysql+pymysql://" + USERNAME + ":" + PASSWORD + "@" + "0.0.0.0" + ":" + PORT + "/" + DATABASE
