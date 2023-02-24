80423987d9a6fded3ce90a3f3e13f0ca1d90d393
DenysPostolnyi <postlden44@gmail.com>
2023-02-24 09:02:38 +0200

    travis db

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,11 @@
 import unittest
 
 from src.service import doctor_service
-from src import create_app
+from src import create_app, DBSettings
 
 
 class TestDB(unittest.TestCase):
     def test_get_doctor(self):
+        print(DBSettings.SQLALCHEMY_DATABASE_URL)
         with create_app().app_context():
             self.assertIsNotNone(doctor_service.get_one_by_id(2))
