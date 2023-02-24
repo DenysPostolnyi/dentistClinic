18af128a1164bf519404ba81f9fea450dbb56322
DenysPostolnyi <postlden44@gmail.com>
2023-02-24 18:17:57 +0200

    replased adding doctor to WITH

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,8 +7,8 @@ from src.service import doctor_service
 
 class TestDB(unittest.TestCase):
     def test_get_doctor(self):
-        doctor = Doctor(full_name="Derek", seniority=4, specialty=Specialty.THERAPIST, phone_number="0636582647",
-                        email="email2@gmail.com")
-        doctor_service.add_doctors(doctor)
         with create_app().app_context():
+            doctor = Doctor(full_name="Derek", seniority=4, specialty=Specialty.THERAPIST, phone_number="0636582647",
+                            email="email2@gmail.com")
+            doctor_service.add_doctors(doctor)
             self.assertIsNotNone(doctor_service.get_all())
