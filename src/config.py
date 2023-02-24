398544677de4e003abb41baa3f6a15720f986339
DenysPostolnyi <postlden44@gmail.com>
2023-02-24 09:41:56 +0200

    created url for travis mysqlserver

--- src/config.py
+++ src/config.py
@@ -11,10 +11,10 @@ class DBSettings:
     """
     class-config for DB
     """
-    DATABASE = os.getenv('MYSQL_DATABASE')
-    USERNAME = os.getenv('MYSQL_USERNAME')
-    PASSWORD = os.getenv('MYSQL_PASSWORD') if os.getenv('MYSQL_PASSWORD') else ""
-    HOST = os.getenv('MYSQL_HOST')
-    PORT = os.getenv('MYSQL_PORT')
-    SQLALCHEMY_DATABASE_URL = "mysql+pymysql://" + USERNAME + ":" + PASSWORD + "@" + HOST + ":" + PORT + "/" + DATABASE \
-        if DATABASE == "db" else "mysql://" + USERNAME + ":" + PASSWORD + "@" + HOST + ":" + PORT + "/" + DATABASE
+    # DATABASE = os.getenv('MYSQL_DATABASE')
+    # USERNAME = os.getenv('MYSQL_USERNAME')
+    # PASSWORD = os.getenv('MYSQL_PASSWORD') if os.getenv('MYSQL_PASSWORD') else ""
+    # HOST = os.getenv('MYSQL_HOST')
+    # PORT = os.getenv('MYSQL_PORT')
+    SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+
