10e5bd78379e1a7a4bb1381b4134e45d9a3b41c8
DenysPostolnyi <postlden44@gmail.com>
2023-02-27 12:25:06 +0200

    logging for doctor API

--- src/__init__.py
+++ src/__init__.py
@@ -1,12 +1,37 @@
 """
 Init file for app
 """
+import logging
+
 from flask import Flask
 from flask_restful import Api
 from src.models.models import db, migrate
 from src.config import DBSettings
 from src.rest.doctor_api import api_doctor
 from src.rest.patient_api import api_patient
+
+# create a logger instance
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# create a file handler that logs messages to a file
+file_handler = logging.FileHandler('logs.log')
+file_handler.setLevel(logging.DEBUG)
+
+# create a console handler that logs messages to the console
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# create a formatter that formats log messages
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add the formatter to the file handler and console handler
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# add the file handler and console handler to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
 
 app = Flask(__name__, instance_relative_config=True)
 app.config['SQLALCHEMY_DATABASE_URI'] = DBSettings.SQLALCHEMY_DATABASE_URL
--- src/rest/doctor_api.py
+++ src/rest/doctor_api.py
@@ -4,6 +4,7 @@ from flask import abort, Flask, Blueprint, request
 from flask_restful import Resource, Api
 from src.service import doctor_service
 from src.util import doctor_mapper
+import logging
 
 app = Flask(__name__)
 api_doctor = Blueprint('doctor_api', __name__)
@@ -14,14 +15,17 @@ class DoctorAPIGetPost(Resource):
     def get(self):
         doctors = doctor_service.get_all()
         if doctors:
+            logging.debug("All doctors was gotten: %s", doctors)
             return [json.loads(obj.to_json()) for obj in doctors]
         else:
+            logging.debug("Tried get all doctors but doctors list is empty")
             abort(404, "Doctors were not found")
 
     def post(self):
         new_doctor = request.get_json(force=True)
         doctor = doctor_mapper.json_to_doctor(new_doctor)
         doctor_service.add_doctors(doctor)
+        logging.debug("New doctor was added to DB: %s", doctor)
         return {"message": "Doctor was added successfully"}
 
 
@@ -29,8 +33,10 @@ class DoctorAPIGetUpdateDelete(Resource):
     def get(self, id):
         try:
             doctor = doctor_service.get_one_by_id(id)
+            logging.debug("Doctor by id - %s was gotten, doctor: %s", id, doctor)
             return json.loads(doctor.to_json())
         except RuntimeError as error:
+            logging.debug("Doctor by id - %s was not found", id)
             abort(404, str(error))
 
     def put(self, id):
@@ -39,15 +45,19 @@ class DoctorAPIGetUpdateDelete(Resource):
         try:
             doctor_service.update(id, doctor)
             doctor.doctor_id = id
+            logging.debug("Doctor by id - %s was updated, new doctor: %s", id, doctor)
             return json.loads(doctor.to_json())
         except RuntimeError as error:
+            logging.debug("Doctor for update by id - %s was not found", id)
             abort(404, str(error))
 
     def delete(self, id):
         try:
             doctor_service.delete(id)
+            logging.debug("Doctor by id - %s was deleted", id)
             return {"message": "Doctor was successfully deleted"}
         except RuntimeError as error:
+            logging.debug("Doctor for delete by id - %s was not found", id)
             abort(404, str(error))
 
 
