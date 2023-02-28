ac196f3361096807e08ed6f9fa4ef7b4743e068f
DenysPostolnyi <postlden44@gmail.com>
2023-02-28 21:24:45 +0200

    created rout for page for adding doctor

--- src/views/doctor_routes.py
+++ src/views/doctor_routes.py
@@ -2,14 +2,14 @@
 Routes for testing DB services
 """
 import requests
-from flask import Blueprint, render_template, abort, redirect, url_for
+from flask import Blueprint, render_template, abort, redirect, url_for, request
 from jinja2 import TemplateNotFound
 
 doctor_routes = Blueprint('doctor_routes', __name__,
                           template_folder='templates')
 
 
-@doctor_routes.route('/doctors')
+@doctor_routes.route('/doctors', methods=['GET'])
 def doctor_index():
     try:
         doctors = requests.get("http://127.0.0.1:5000/doctor-api").json()
@@ -24,17 +24,36 @@ def doctor_index():
         abort(404)
 
 
-@doctor_routes.route("/doctors/delete/<int:doctor_id>")
+@doctor_routes.route("/doctors/delete/<int:doctor_id>", methods=['GET'])
 def delete(doctor_id):
-    request = requests.delete(f"http://127.0.0.1:5000/doctor-api/{doctor_id}")
-    return redirect(url_for('doctor_routes.doctor_index'))
+    try:
+        request = requests.delete(f"http://127.0.0.1:5000/doctor-api/{doctor_id}")
+        return redirect(url_for('doctor_routes.doctor_index'))
+    except TemplateNotFound:
+        abort(404)
 
 
-@doctor_routes.route("/doctors/<int:doctor_id>")
+@doctor_routes.route("/doctors/<int:doctor_id>", methods=['GET'])
 def info(doctor_id):
-    request = requests.get(f"http://127.0.0.1:5000/doctor-api/{doctor_id}")
-    if request.status_code == 200:
-        doctor = request.json()
-        doctor['specialty'] = doctor['specialty']
-        return render_template("doctor/doctorInfo.html", doctor = doctor)
-    return redirect(url_for('doctor_routes.doctor_index'))
+    try:
+        request = requests.get(f"http://127.0.0.1:5000/doctor-api/{doctor_id}")
+        if request.status_code == 200:
+            doctor = request.json()
+            doctor['specialty'] = doctor['specialty']
+            return render_template("doctor/doctorInfo.html", doctor=doctor)
+        return redirect(url_for('doctor_routes.doctor_index'))
+    except TemplateNotFound:
+        abort(404)
+
+
+@doctor_routes.route("/doctors/add", methods=['GET', 'POST'])
+def add():
+    try:
+        if request.method == 'GET':
+            return render_template("doctor/doctorAdd.html")
+        new_doctor = request.form
+        req = requests.post("http://127.0.0.1:5000/doctor-api", json=new_doctor).json()
+        print(req)
+        return redirect(url_for('doctor_routes.info', doctor_id=req['doctor']['doctor_id']))
+    except TemplateNotFound:
+        abort(404)
