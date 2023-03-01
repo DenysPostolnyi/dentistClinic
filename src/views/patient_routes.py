ef8ff84718b1a28770291e6e8887b3a81afb2673
DenysPostolnyi <postlden44@gmail.com>
2023-03-01 14:23:02 +0200

    created page for adding new patient

--- src/views/patient_routes.py
+++ src/views/patient_routes.py
@@ -2,7 +2,7 @@
 Routes for testing DB services
 """
 import requests
-from flask import Blueprint, render_template, abort, redirect, url_for
+from flask import Blueprint, render_template, abort, redirect, url_for, request
 from jinja2 import TemplateNotFound
 
 patient_routes = Blueprint('patient_routes', __name__,
@@ -32,9 +32,27 @@ def delete_patient(patient_id):
 
 @patient_routes.route("/patients/<int:patient_id>")
 def info(patient_id):
-    request = requests.get(f"http://127.0.0.1:5000/patient-api/{patient_id}")
-    if request.status_code == 200:
-        patient = request.json()
+    req = requests.get(f"http://127.0.0.1:5000/patient-api/{patient_id}")
+    if req.status_code == 200:
+        print("<===============+++>")
+        # print(url_for(request.endpoint, patient_id=request.args.get('patient_id')))
+
+        print("<===============+++>")
+        patient = req.json()
         patient['kind_of_ache'] = patient['kind_of_ache']
         return render_template("patient/patientInfo.html", patient=patient)
     return redirect(url_for('patient_routes.patient_index'))
+
+
+@patient_routes.route("/patients/add", methods=['GET', 'POST'])
+def add():
+    try:
+        if request.method == 'GET':
+            return render_template("patient/patientAdd.html")
+        new_patient = request.form
+        req = requests.post("http://127.0.0.1:5000/patient-api", json=new_patient)
+        if req.status_code == 200:
+            return redirect(url_for('patient_routes.info', patient_id=req.json()['patient']['patient_id']))
+        return redirect(url_for('patient_routes.patient_index'))
+    except TemplateNotFound:
+        abort(404)
